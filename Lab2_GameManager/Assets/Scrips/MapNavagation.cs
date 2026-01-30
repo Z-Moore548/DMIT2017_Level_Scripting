bbdb5845ed8ec25d4fc413fcad799bee9ac02eb2
Z-Moore548 <zmoore1@nait.ca>
2026-01-30 11:25:45 -0700

    Finished Map (Mostly could go back and make areas better)

--- Lab2_GameManager/Assets/Scrips/MapNavagation.cs
+++ Lab2_GameManager/Assets/Scrips/MapNavagation.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class MapNavagation : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class MapNavagation : MonoBehaviour
     [SerializeField] private Transform mapParent;
     private Dictionary<int, MapData> mapDictionary = new Dictionary<int, MapData>();
     [SerializeField] private GameObject currentMap;
+    public UnityEvent OnMapEnter;
     private void Awake()
     {
         if(Instance == null) Instance = this;
@@ -38,10 +40,16 @@ public class MapNavagation : MonoBehaviour
 
 
         Destroy(currentMap);
-
+        
         currentMap = Instantiate(mapDictionary[mapID].prefab, mapParent);
         Grid g = mapParent.GetComponent<Grid>();
         player.position = g.GetCellCenterWorld(mapDictionary[mapID].entryPoints[portalID].cell);
+        OnMapEnter?.Invoke();
+        // get the cell that we want to the player to spawn in mapDictionary[mapID].entryPoints[portalID].cell
+        // convert the cell into world space. returns a Vector3 <-- the new position
+        // set the position to the updated value
+
+        // add aesthetics: screen fade, sfx, animations
     }
 }
 
